0dde8ed3c6cfbda48eb93026a36f6d413cec85ed
Iván Corrales Solera <developer@wesovilabs.com>
2018-11-16 08:10:07 +0100

    feat: working on improving performance for map operation

--- internal/reduce/dispatcher.go
+++ internal/reduce/dispatcher.go
@@ -9,17 +9,16 @@ func dispatch(items *reflect.Value, fn interface{}, info *reduceInfo) interface{
 	switch info.fnIn2Type.Kind() {
 	case reflect.Int:
 		items := items.Interface().([]int)
-		switch info.fnIn2Type.Kind() {
+		switch info.fnIn1Type.Kind() {
 		case reflect.Int:
 			return reduceIntToInt(items, fn.(func(int, int) int))
 		}
 	case reflect.String:
 		items := items.Interface().([]string)
-		switch info.fnIn2Type.Kind() {
+		switch info.fnIn1Type.Kind() {
 		case reflect.Int:
 			return reduceStringToInt(items, fn.(func(int, string) int))
 		case reflect.String:
-			fmt.Printf("%#v",info)
 			return reduceStringToString(items, fn.(func(string, string) string))
 		}
 
